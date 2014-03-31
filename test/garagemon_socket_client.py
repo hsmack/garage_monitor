91559455a5eb9b9ba88de5fb19bf0bbd7044b124
hsmack <henryc@gmail.com>
2014-03-30 20:57:47 -0700

    working paired copy of server architecture

--- test/garagemon_socket_client.py
+++ test/garagemon_socket_client.py
@@ -2,18 +2,29 @@
 
 # client program
 import socket
+import time
 
-HOST = 'garage.local'    # The remote host
-PORT = 4001              # The same port as used by the server
+# HOST = 'tv.local'    # The remote host
+#
+# HOST = the remote host
+# PORT = the port of the remote host
+#
+HOST = '127.0.0.1'
+PORT = 4003
 
 for i in xrange(4):
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   s.connect((HOST, PORT))
-  # data = '0,OPEN,2013-12-13 14:11:11'
-  data = 'get_state'
+  data = '0,OPEN,2013-12-13 14:11:11' + str(i)
   print 'sending payload ...', repr(data)
   s.sendall(data)
-  data = s.recv(1024)
-  s.close()
-  print 'Received', repr(data)
+  
+  #
+  # optional server reply
+  # 
+  # data = s.recv(1024)
+  # print 'Received', repr(data)  # if there's a server reply
 
+  s.close()
+  time.sleep(1)
+  print "sleep..."
